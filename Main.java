d7dd02f70cd055feaf5e1f2f63dfdc68ed11d677
Drsoromario <93512624+Drsoromario@users.noreply.github.com>
2024-09-14 02:26:17 +0900

    Main.javaファイルを作成
    git fetchコマンドのテストをするためにこのファイルを作成

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.print("数値を入力してください:");
+    int a = java.util.Scanner(System.in).nextInt();
+    System.out.println("入力された数の平方数は" + a*a + "です\n");
+  }
+}
